f4e38d315f21fcad0fd9bac24c1d9817a86f02b8
Rayiumir <swordart2050@gmail.com>
2025-07-05 03:57:16 +0330

    Create Model Post

--- app/Models/Discussion.php
+++ app/Models/Discussion.php
@@ -12,9 +12,25 @@ class Discussion extends Model
         return $this->belongsTo(Topic::class);
     }
 
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function isPinned(): bool
     {
         return !is_null($this->pinned_at);
     }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function post()
+    {
+        return $this->hasOne(Post::class)->whereNull('parent_id');
+    }
+
 
 }
---
+++ app/Models/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'body'
+    ];
+
+    public function discussion(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Discussion::class);
+    }
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
