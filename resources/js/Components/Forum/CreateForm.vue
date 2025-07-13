9f2a247ad890f270169cb0db8c757ca6bdc32c1f
Rayiumir <swordart2050@gmail.com>
2025-07-13 19:13:55 +0330

    Keeping form state

--- resources/js/Components/Forum/CreateForm.vue
+++ resources/js/Components/Forum/CreateForm.vue
@@ -2,7 +2,7 @@
     <FixedForm v-if="visible">
         <template v-slot:header>
             <div class="flex items-center justify-between">
-                <h1 class="text-lg font-medium">New siscussion</h1>
+                <h1 class="text-lg font-medium">New discussion</h1>
                 <button v-on:click="hideCreateDiscussion">&times;</button>
             </div>
         </template>
@@ -11,22 +11,26 @@
                 <div class="flex-grow">
                     <div>
                         <InputLabel for="Title" value="Title" class="sr-only"></InputLabel>
-                        <TextInput id="Title" type="text" class="w-full"></TextInput>
-                        <InputError class="mt-2"></InputError>
+                        <TextInput id="Title" type="text" class="w-full" placeholder="Discussion Title" v-model="form.title"></TextInput>
+                        <InputError class="mt-2" :message="form.errors.title"></InputError>
                     </div>
                 </div>
                 <div>
-                    <Select id="topic">
-                        <option value="">Choose a topic</option>
-                        <option :value="topic.slug" v-for="topic in $page.props.topics" :key="topic.id">
+                    <InputLabel for="topic" value="Topic" class="sr-only"></InputLabel>
+                    <select id="topic" v-model="form.topic_id" class="rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500">
+                        <option value="">Choose a topic ...</option>
+                        <option :value="topic.id" v-for="topic in $page.props.topics" :key="topic.id">
                             {{ topic.title }}
                         </option>
-                    </Select>
+                    </select>
+                    <InputError class="mt-2" :message="form.errors.topic_id"></InputError>
                 </div>
 
             </div>
             <div class="mt-4">
-                <Textarea name="" id="" cols="6" rows="4" class="w-full" placeholder="Body"></Textarea>
+                <InputLabel for="body" value="Body" class="sr-only"></InputLabel>
+                <Textarea name="" id="" cols="6" rows="4" class="w-full" v-model="form.body"></Textarea>
+                <InputError class="mt-2" :message="form.errors.body"></InputError>
             </div>
         </template>
         <template v-slot:button>
@@ -39,10 +43,10 @@
     import InputLabel from "@/Components/InputLabel.vue";
     import TextInput from "@/Components/TextInput.vue";
     import InputError from "@/Components/InputError.vue";
-    import Select from "@/Components/Select.vue";
+    // import Select from "@/Components/Select.vue";
     import Textarea from "@/Components/Textarea.vue";
     import PrimaryButton from "@/Components/PrimaryButton.vue";
     import useCreateDiscussion from "@/Composables/useCreateDiscussion";
 
-    const { visible } = useCreateDiscussion()
+    const { form, visible, hideCreateDiscussion } = useCreateDiscussion()
 </script>
--- resources/js/Composables/useCreateDiscussion.js
+++ resources/js/Composables/useCreateDiscussion.js
@@ -1,6 +1,12 @@
-import {ref} from 'vue'
+import { ref } from 'vue';
+import { useForm } from "@inertiajs/vue3";
 
 const  visible = ref(false)
+const form = useForm({
+    topic_id: '',
+    title: '',
+    body: ''
+})
 export default () => {
     const showCreateDiscussion = () => {
         visible.value = true
@@ -9,8 +15,9 @@ export default () => {
         visible.value = false
     }
     return{
+        form,
         visible,
-        showCreateDiscussion,
-        hideCreateDiscussion
+        hideCreateDiscussion,
+        showCreateDiscussion
     }
 }
